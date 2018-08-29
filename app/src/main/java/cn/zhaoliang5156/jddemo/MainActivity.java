2946098129e8e3faa1e4cc5969090e69af792049
zhaoliang <2668645098@qq.com>
2018-08-29 17:45:31 +0800

    开发了一点小功能

--- app/src/main/java/cn/zhaoliang5156/jddemo/MainActivity.java
+++ app/src/main/java/cn/zhaoliang5156/jddemo/MainActivity.java
@@ -3,6 +3,13 @@ package cn.zhaoliang5156.jddemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 这里是开发版本
+ *
+ * @author zhaoliang
+ * @version 1.0
+ * @create 2018/8/29
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
